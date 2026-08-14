ee4074f9bb75046cf53eed994f5744bb20fa14b2
sntnkillian-design <sntnkillian@gmail.com>
2026-08-15 00:40:05 +0200

    feat: switch application entry to V1.3

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import AppV12 from './AppV12';
+import AppV13 from './AppV13';
 
-createRoot(document.getElementById('root')!).render(<React.StrictMode><AppV12/></React.StrictMode>);
+createRoot(document.getElementById('root')!).render(<React.StrictMode><AppV13/></React.StrictMode>);
