43bde487a98470ed2c22fa2da678c507a33267c5
sntnkillian-design <sntnkillian@gmail.com>
2026-08-15 01:45:23 +0200

    feat: activate V1.5 homepage art direction

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import AppV14 from './AppV14';
 import './v14-fix.css';
+import './v15.css';
 
 createRoot(document.getElementById('root')!).render(<React.StrictMode><AppV14/></React.StrictMode>);
